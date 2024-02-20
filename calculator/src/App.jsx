e9fb99b35f895cd5fdf4f91994f15a01c32905b6
Eduardo Pineda Luis <isc20350307@gmail.com>
2024-02-20 12:04:41 -0600

    v3

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css'
 
 const numericbutonsclases = 'btn btn-outline-warning w-100';
@@ -5,6 +6,45 @@ const operatorbuttonsclases = 'btn btn-outline-primary w-100';
 const especialbuttonsclases = 'btn btn-outline-danger w-100';
 
 function App() {
+
+  const [display, setDisplay] = useState({
+    value: '0', 
+    hasPoint: false,
+  })
+
+  const updateDisplay = (value) => {
+    if (value === '.'){
+      if(display.hasPoint){
+        return
+    }
+    setDisplay({
+      ...display,
+      value: display.value + value,
+      hasPoint: true,
+    })
+    return
+  }
+    if(display.value === '0'){
+      setDisplay({
+        ...display,
+        value: value,
+      })
+      return
+    }
+setDisplay({
+  ...display,
+  value: display.value + value,
+})
+  }
+
+  const clearDisplay = () => { 
+    setDisplay({
+      ...display,
+      value: '0',
+      hasPoint: false,
+    })
+    
+  }
 
   return (
     <div>
@@ -15,28 +55,33 @@ function App() {
 
           <tr>
             <td className='text-end' colSpan={4}>
-              <h2>0</h2>
+              <h2>{display.value}</h2>
             </td>
             
           </tr>
           <tr>
             <td>
-            <button className={especialbuttonsclases}             type='button'>
+            <button className={especialbuttonsclases}             
+            type='button'
+            onClick={clearDisplay}>
               C
              </button>
             </td>
             <td>
-            <button className={especialbuttonsclases}             type='button'>
+            <button className={especialbuttonsclases}             
+            type='button'>
               {'<'}
              </button>
             </td>
             <td>
-            <button className={operatorbuttonsclases}             type='button'>
+            <button className={operatorbuttonsclases}             
+            type='button'>
               %
              </button>
             </td>
             <td>
-            <button className={operatorbuttonsclases}             type='button'>
+            <button className={operatorbuttonsclases}             
+            type='button'>
               /
              </button>
             </td>
@@ -44,24 +89,28 @@ function App() {
           <tr>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('7')}>
               7
              </button>
             </td>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('8')}>
               8
              </button>
             </td>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('9')}>
               9
              </button>
             </td>
             <td>
-            <button className={operatorbuttonsclases}             type='button'>
+            <button className={operatorbuttonsclases}             
+            type='button'>
               x
              </button>
             </td>
@@ -69,64 +118,80 @@ function App() {
           <tr>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('4')}>
               4
              </button>
             </td>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('5')}>
               5
              </button>
             </td>
             <td>
             <button className={numericbutonsclases}
-             type='button'>
+             type='button'
+             onClick={() => updateDisplay('6')}>
               6
              </button>
             </td>
             <td>
-            <button className={operatorbuttonsclases}             type='button'>
+            <button className={operatorbuttonsclases}          
+               type='button'>
               -
              </button>
             </td>
           </tr>
           <tr>
             <td>
-            <button className={numericbutonsclases}             type='button'>
+            <button className={numericbutonsclases}            
+             type='button'
+             onClick={() => updateDisplay('1')}>
               1
              </button>
             </td>
             <td>
-            <button className={numericbutonsclases}             type='button'>
+            <button className={numericbutonsclases}           
+              type='button'
+              onClick={() => updateDisplay('2')}>
               2
              </button>
             </td>
             <td>
-            <button className={numericbutonsclases}             type='button'>
+            <button className={numericbutonsclases} 
+            type='button'
+            onClick={() => updateDisplay('3')}>
               3
              </button>
             </td>
             <td>
-            <button className={operatorbuttonsclases}             type='button'>
+            <button className={operatorbuttonsclases}    
+            type='button'>
               +
              </button>
             </td>
           </tr>
           <tr>
             <td colSpan={2}>
-            <button className={numericbutonsclases}             type='button'>
+            <button className={numericbutonsclases}     
+             type='button'
+             onClick={() => updateDisplay('0')}>
               0
              </button>
             </td>
 
             <td>
-            <button className={numericbutonsclases}             type='button'>
+            <button className={numericbutonsclases}            
+             type='button'
+             onClick={() => updateDisplay('.')}>
               .
              </button>
             </td>
             <td>
-            <button className={especialbuttonsclases}             type='button'>
+            <button className={especialbuttonsclases}             
+            type='button'>
               =
              </button>
             </td>
